9dc6e0d62af781d72fd3ee2bee02902da1a77c23
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-10-28 12:07:17 +0800

    kernel: Import BORE (Burst-Oriented Response Enhancer) CPU Scheduler 5.1.0 form firelzrd/bore-scheduler
    Co-authored-by: Rve27 <rve27github@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2241,6 +2241,9 @@ static __latent_entropy struct task_struct *copy_process(
 		fd_install(pidfd, pidfile);
 
 	proc_fork_connector(p);
+#ifdef CONFIG_SCHED_BORE
+	sched_post_fork(p);
+#endif // CONFIG_SCHED_BORE
 	cgroup_post_fork(p);
 	cgroup_threadgroup_change_end(current);
 	perf_event_fork(p);
